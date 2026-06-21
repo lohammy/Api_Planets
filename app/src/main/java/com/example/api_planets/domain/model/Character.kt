c16fae9906009903510e2cf081cbe30e8a5c6632
2005lohammy <2005lohammy@gmail,com>
2026-06-21 19:19:37 -0400

    crear Character.kt

---
+++ app/src/main/java/com/example/api_planets/domain/model/Character.kt
@@ -0,0 +1,12 @@
+package com.example.api_planets.domain.model
+
+data class Character(
+    val id: Int,
+    val name: String,
+    val ki: String,
+    val race: String,
+    val gender: String,
+    val description: String,
+    val image: String,
+    val maxKi: String
+)
