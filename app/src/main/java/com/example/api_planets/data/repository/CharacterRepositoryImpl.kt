50498428989001a98f7fa14c99bc9811d179370c
2005lohammy <2005lohammy@gmail,com>
2026-06-21 19:22:13 -0400

    crear CharacterRepositoryImpl.kt

---
+++ app/src/main/java/com/example/api_planets/data/repository/CharacterRepositoryImpl.kt
@@ -0,0 +1,54 @@
+package com.example.api_planets.data.repository
+
+import com.example.api_planets.data.remote.DragonBallApi
+import com.example.api_planets.data.remote.Resource
+import com.example.api_planets.domain.model.Character
+import com.example.api_planets.domain.repository.CharacterRepository
+import jakarta.inject.Inject
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class CharacterRepositoryImpl @Inject constructor(
+    private val api: DragonBallApi,
+) : CharacterRepository {
+
+    override fun getCharacters(
+        page: Int,
+        limit: Int,
+        name: String?,
+        gender: String?,
+        race: String?
+    ): Flow<Resource<List<Character>>> = flow {
+        emit(Resource.Loading())
+        try {
+            val response = api.getCharacters(page, limit, name, gender, race)
+            if (response.isSuccessful) {
+                val characters = response.body()?.items?.map { it.toDomain() } ?: emptyList()
+                emit(Resource.Success(characters))
+            } else {
+                emit(Resource.Error(response.message() ?: "Error desconocido"))
+            }
+        } catch (e: Exception) {
+            emit(Resource.Error(e.message ?: "Error de red"))
+        }
+    }
+
+    override fun getCharacterDetail(id: Int): Flow<Resource<Character>> = flow {
+        emit(Resource.Loading())
+        try {
+            val response = api.getCharacterDetail(id)
+            if (response.isSuccessful) {
+                val character = response.body()?.toDomain()
+                if (character != null) {
+                    emit(Resource.Success(character))
+                } else {
+                    emit(Resource.Error("Personaje no encontrado"))
+                }
+            } else {
+                emit(Resource.Error(response.message() ?: "Error desconocido"))
+            }
+        } catch (e: Exception) {
+            emit(Resource.Error(e.message ?: "Error de red"))
+        }
+    }
+}
